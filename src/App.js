d83d234cf2d7c94be484a098ff354aab2e7ad19a
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-04-16 15:41:39 -0700

    added cart to local storage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Route } from 'react-router-dom'
 import data from './data'
 import ProductContext from './contexts/ProductContext'
 import CartContext from './contexts/CartContext'
-
+import useLocalStorage from './hooks/localStorage'
 // Components
 import Navigation from './components/Navigation'
 import Products from './components/Products'
@@ -12,6 +12,18 @@ import ShoppingCart from './components/ShoppingCart'
 function App() {
 	const [products] = useState(data)
 	const [cart, setCart] = useState([])
+
+	const [storedCart, setStoredCart] = useLocalStorage('storedCart', cart)
+
+	// get cart from localstorage at the beginning
+	useEffect(() => {
+		setCart(storedCart)
+	}, [])
+
+	// set update stored cart whenever cart changes
+	useEffect(() => {
+		setStoredCart(cart)
+	}, [cart])
 
 	const addItem = item => {
 		// add the given item to the cart
